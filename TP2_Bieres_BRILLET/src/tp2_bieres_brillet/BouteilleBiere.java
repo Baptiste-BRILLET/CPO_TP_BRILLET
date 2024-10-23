d2e3b9e49d65c8b29778643d016fea50b98fe305
baptistebrillet <baptistebrillet@localhost>
2024-10-23 12:20:40 +0200

    TP2_Bières_BRILLET

--- TP2_Bieres_BRILLET/src/tp2_bieres_brillet/BouteilleBiere.java
+++ TP2_Bieres_BRILLET/src/tp2_bieres_brillet/BouteilleBiere.java
@@ -25,12 +25,28 @@ public BouteilleBiere(String unNom, float unDegre, String uneBrasserie) {
 
     // Méthode lireEtiquette
 public void lireEtiquette() {
-    System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degrés)\nBrasserie : " + brasserie);
+    System.out.println("Bouteille de " + nom + " (" + degreAlcool + " degrés)\nBrasserie : " + brasserie); 
 }
+public boolean Decapsuler() {
+        if (!ouverte) {
+            ouverte = true;
+            System.out.println("Bouteille de " + nom + " décapsulée.");
+            return true;  // La méthode renvoie true si la bouteille est décapsulée avec succès
+        } else {
+            System.out.println("Erreur : bière déjà ouverte.");
+            return false;  // La méthode renvoie false si la bouteille était déjà ouverte
+        }
 }
-
-
-
-
-  
+ @Override
+    public String toString() {
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + " (" + degreAlcool + " degrés) - Brasserie : " + brasserie + " - Ouverte ? ";
+        if (ouverte) {
+            chaine_a_retourner += "oui";
+        } else {
+            chaine_a_retourner += "non";
+        }
+        return chaine_a_retourner;
+    }
+}
 
--- TP2_Bieres_BRILLET/src/tp2_bieres_brillet/TP2_Bieres_BRILLET.java
+++ TP2_Bieres_BRILLET/src/tp2_bieres_brillet/TP2_Bieres_BRILLET.java
@@ -5,7 +5,7 @@
 package tp2_bieres_brillet;
 
 /**
- *
+ *Baptiste BRILLET TDC
  * @author baptistebrillet
  */
 public class TP2_Bieres_BRILLET {
@@ -15,25 +15,21 @@ public class TP2_Bieres_BRILLET {
      */
     public static void main(String[] args) {
        // Création Cuvée des Trolls
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", (float) 7.0 ,"Dubuisson") ;
-
-        uneBiere.lireEtiquette();
-        // Création Leffe
-        BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe", (float) 6.6 ,"Abbaye de Leffe");
-     
-        deuxiemeBiere.lireEtiquette();
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des Trolls", 7.0f, "Dubuisson");
+        BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe", 6.6f, "Abbaye de Leffe");
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("Kronenbourg", 5.3f, "Brasserie Kronenbourg");  
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Heineken", 5.0f, "Schiltigheim");
+        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Desperados", 9.0f, "Alken-Maes");
         
-        BouteilleBiere troisiemeBiere = new BouteilleBiere("Kronenbourg", (float) 5.3 ,"Brasserie Kronenbourg") ;
+         // Décapsuler la première et la deuxième bières
+        uneBiere.Decapsuler();
+        deuxiemeBiere.Decapsuler();
 
-        troisiemeBiere.lireEtiquette();
-        
-        BouteilleBiere quatriemeBiere = new BouteilleBiere("Heineken", (float) 5.0 ,"Schiltigheim") ;
-
-        quatriemeBiere.lireEtiquette();
-        
-        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Desperados", (float) 9.0 ,"Alken-Maes") ;
-
-        cinquiemeBiere.lireEtiquette();
+        // Utiliser System.out.println() pour afficher les bières avec la méthode toString()
+        System.out.println(uneBiere);       // Bière Cuvée des Trolls devrait être affichée comme ouverte
+        System.out.println(deuxiemeBiere);  // Bière Leffe devrait être affichée comme ouverte
+        System.out.println(troisiemeBiere); // Bière Kronenbourg devrait être affichée comme fermée
+        System.out.println(quatriemeBiere); // Bière Heineken devrait être affichée comme fermée
+        System.out.println(cinquiemeBiere); // Bière Desperados devrait être affichée comme fermée
     }
-    
 }
